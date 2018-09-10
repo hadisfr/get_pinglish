8517acee9fab80e1fb147e874fc90bc24788327e
h@di <hadi@hadisafari.ir>
2018-09-10 22:28:58 +0430

    add call_api

--- get_pinglish.py
+++ get_pinglish.py
@@ -3,10 +3,8 @@ import json
 from random import randint
 
 
-endpoint = "http://www.virastlive.com/trans.php"
-
-
-def get_pinglish_from_word(word, full_res):
+def call_api(text):
+    endpoint = "http://www.virastlive.com/trans.php"
     res = requests.post(
         endpoint,
         headers={"Accept": "application/json", "charset": "UTF-8"},
@@ -14,7 +12,7 @@ def get_pinglish_from_word(word, full_res):
             "id": randint(1, 99),
             "method": "Transliterate",
             "params": {
-                "text": word,
+                "text": repr(text)[1:-1],
                 "option": {
                     "SourceAlphabet": "Persian",
                     "DestinationAlphabet": "2OM",
@@ -27,10 +25,14 @@ def get_pinglish_from_word(word, full_res):
         if not res.text or res.text == "VirexTransError":
             raise RuntimeError(res.text)
         else:
-            suggestions = res.json()["result"][1][0]["suggestions"]
-            return suggestions if full_res else suggestions[0]
+            return res.json()["result"][1]
     else:
         raise RuntimeError("%d: %s" % (res.status_code, res.text))
+
+
+def get_pinglish_from_word(word, full_res):
+    suggestions = call_api(word)[0]["suggestions"]
+    return suggestions if full_res else suggestions[0]
 
 
 def get_pinglish(word, full_res=False):
